8735254e9587871b24e2eaddac337d7f38ac1eb8
Fauzan Naufal Ihsan Dhuha <fdhuha17@gmail.com>
2023-02-21 22:21:15 +0700

    SetupProject done

---
+++ projects/server/controllers/adminController.js
@@ -0,0 +1,10 @@
+const {sequelize} = require('./../models')
+
+const db = require('./../models/index')
+const admin = db.admin
+
+module.exports = {
+    login: async(req, res) => {
+
+    }
+}
---
+++ projects/server/models/branch_store.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class branch_store extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      branch_store.belongsTo(models.admin, {
+        foreignKey: 'admin_id'
+      })
+      branch_store.hasMany(models.branch_products, {
+        foreignKey: 'branch_id'
+      })
+    }
+  }
+  branch_store.init({
+    branch_name: DataTypes.STRING,
+    address: DataTypes.STRING,
+    longitude: DataTypes.STRING,
+    latitude: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'branch_store',
+  });
+  return branch_store;
+};
